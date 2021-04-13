acb016dc7ffc15f699c2e2f3021c9721c8d1c344
diana <dianasequeirosf@gmail.com>
2021-04-13 15:25:35 +0200

    Add NavigationDrawer/Menu

--- TuAppGenda/app/src/main/java/com/example/tuappgenda/screens/home/HomeActivity.java
+++ TuAppGenda/app/src/main/java/com/example/tuappgenda/screens/home/HomeActivity.java
@@ -1,16 +1,37 @@
 package com.example.tuappgenda.screens.home;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 import com.example.tuappgenda.R;
 
 public class HomeActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.idToolBar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.idDrawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, 0, 0);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
     }
 }
