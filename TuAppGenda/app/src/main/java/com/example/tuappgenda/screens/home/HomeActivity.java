ca23dd12986ecaedcd22d822ae909836b3d48ac7
diana <dianasequeirosf@gmail.com>
2021-04-13 16:58:41 +0200

    Add sections fragments to Menu

--- TuAppGenda/app/src/main/java/com/example/tuappgenda/screens/home/HomeActivity.java
+++ TuAppGenda/app/src/main/java/com/example/tuappgenda/screens/home/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.tuappgenda.screens.home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,8 +8,15 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.tuappgenda.R;
+import com.example.tuappgenda.screens.profile.ProfileFragment;
+import com.example.tuappgenda.screens.school.SchoolFragment;
+import com.example.tuappgenda.screens.start.StartFragment;
+import com.example.tuappgenda.screens.subject.SubjectFragment;
+import com.example.tuappgenda.screens.teacher.TeacherFragment;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
@@ -26,6 +34,45 @@ public class HomeActivity extends AppCompatActivity {
                 this, drawerLayout, toolbar, 0, 0);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.idHome_FrameLayout, new StartFragment())
+                .commit();
+
+        NavigationView navigationView = findViewById(R.id.idNavigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.idStart:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.idHome_FrameLayout, new StartFragment()).commit();
+                        break;
+                    case R.id.idProfile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.idHome_FrameLayout, new ProfileFragment()).commit();
+                        break;
+                    case R.id.idSubject:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.idHome_FrameLayout, new SubjectFragment()).commit();
+                        break;
+                    case R.id.idTeacher:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.idHome_FrameLayout, new TeacherFragment()).commit();
+                        break;
+                    case R.id.idSchool:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.idHome_FrameLayout, new SchoolFragment()).commit();
+                        break;
+                    case R.id.idGitHub:
+                        //TODO:
+                        break;
+                    case R.id.idLogout:
+                        //TODO:
+                        break;
+                }
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }
+                return true;
+            }
+        });
     }
 
     @Override
@@ -34,4 +81,5 @@ public class HomeActivity extends AppCompatActivity {
             drawerLayout.closeDrawer(GravityCompat.START);
         }
     }
+
 }
